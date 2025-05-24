ff9c5c55ccaa9612fd464fc21ba4669858aaf506
johannes <johann954@hotmail.com>
2025-05-24 01:14:06 -0500

    feat: mejoras en el nav, nombre de usuario

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,31 @@
+import { LogOut } from "lucide-react";
 import React from "react";
 
 const Navbar = () => {
   return (
     <>
-      <div className="w-full bg-pantone-7738">
-        <div className="max-w-screen-xl mx-auto px-8 py-4">
-          <div className="text-white">
+      <div className="w-full bg-pantone-7738 text-white">
+        <div className="max-w-screen-xl mx-auto px-8 py-4 flex flex-wrap justify-between items-center gap-4">
+          <div>
             <h1 className="text-xl font-bold">SELv2</h1>
             <p className="text-xs">Sistema en Línea</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <p>Dashboard</p>
+            <div className="flex gap-2 items-center">
+              <span className="w-8 h-8 bg-pantone-368 rounded-full flex items-center justify-center  font-medium">
+                J
+              </span>
+              <div className="flex flex-col items-start justify-center text-sm leading-tight">
+                <p>Juan Pérez</p>
+                <p className="text-white/60 text-[0.75rem] sm:text-sm">
+                  Cooperativa Agrícola El Progreso
+                </p>
+              </div>
+            </div>
+            <button className="w-34 bg-red-500 text-white py-2 text-sm rounded-md flex items-center justify-center gap-2 hover:bg-red-600 cursor-pointer">
+              Cerrar sesión <LogOut size={16} />
+            </button>
           </div>
         </div>
       </div>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import LoginForm from "../pages/LoginForm";
 import EvaluationPanel from "../pages/ EvaluationPanel";
+import SistRepTecFin from "../pages/SistRepTecFin";
+import { DashPasosCriticos } from "../pages/DashPasosCriticos";
 
 export const AppRouter = () => {
   return (
@@ -11,6 +13,8 @@ export const AppRouter = () => {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<LoginForm />} />
           <Route path="/evaluator" element={<EvaluationPanel />} />
+          <Route path="/sistema-reporte-tecnico" element={<SistRepTecFin />} />
+          <Route path="/pasos-criticos" element={<DashPasosCriticos />} />
         </Route>
       </Routes>
     </>
